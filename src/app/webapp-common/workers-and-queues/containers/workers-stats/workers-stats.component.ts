6c6ff2d5be713f10c67bd7462e4ff9cee2f8c6c8
lapo <lapo.bartolacci97@gmail.com>
2024-09-02 21:31:33 +0200

    debug from date

--- src/app/webapp-common/workers-and-queues/containers/workers-stats/workers-stats.component.ts
+++ src/app/webapp-common/workers-and-queues/containers/workers-stats/workers-stats.component.ts
@@ -202,6 +202,7 @@ export class WorkersStatsComponent implements OnInit, OnDestroy {
     width = Math.min(0.8 * width, 1000);
     this.liveChart = false;
     this.store.dispatch(getWorkers({date: this.currentDate, maxPoints: width, usePredefinedRange: true}));
+    this.chartChanged();
 
   }
 
@@ -210,5 +211,7 @@ export class WorkersStatsComponent implements OnInit, OnDestroy {
     let width = this.chartRef.element.nativeElement.clientWidth || 1000;
     width = Math.min(0.8 * width, 1000);
     this.store.dispatch(getWorkers({date: this.currentDate, maxPoints: width}));
+    this.chartChanged();
+
   }
 }
